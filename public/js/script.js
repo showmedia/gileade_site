8083de1f52b8b3c4f482357b4e7724ee36bddc46
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-04-25 16:30:12 -0300

    adicionando contact

--- public/js/script.js
+++ public/js/script.js
@@ -18,6 +18,8 @@ const halfScreenHeight = window.innerHeight / 2;
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     nav.classList.add('nav-fixed');
+  }else{
+    nav.classList.remove('nav-fixed');
   }
  
 });
