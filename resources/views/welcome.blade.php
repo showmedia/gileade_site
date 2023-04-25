3936c0db9d5ca05e057375f85447c9b248356e62
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-04-25 15:44:48 -0300

    adicionando contact

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="f-right">
             <h1 id="anim-typewriter"></h1>
             <form class="fade-in-up" action="/consultar" method="post">
-                <input type="text" name="placa" placeholder="Placa/renavam do veículo" required id="placa">
+                <input type="text" name="placa" placeholder="Placa/renavam" required id="placa">
                 <input type="text" name="celular" placeholder="Seu Celular" required id="celular">
                 <button><ion-icon name="search-outline"></ion-icon> Consultar</button>
             </form>
